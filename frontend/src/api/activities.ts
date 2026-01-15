8485e579dbe67c9a9a9094c22b59eb09b47bdc66
Elena <elenavigoolivan@gmail.com>
2026-01-15 20:13:43 +0100

    Fix parameters lint error

--- frontend/src/api/activities.ts
+++ frontend/src/api/activities.ts
@@ -17,12 +17,9 @@ export type PaginatedResponse = {
   results: Activity[];
 };
 
-export async function getActivities(page: {
-  page?: number;
-}): Promise<Activity[]> {
-  const endpoint = page
-    ? `http://127.0.0.1:8000/api/activities/?page=${page}`
-    : 'http://127.0.0.1:8000/api/activities/';
+export async function getActivities(
+  endpoint = 'http://127.0.0.1:8000/api/activities/'
+): Promise<PaginatedResponse> {
   const res = await axios.get(endpoint);
-  return res.data.results;
+  return res.data;
 }
